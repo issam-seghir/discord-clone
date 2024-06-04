f8187917392a2227a9571a285bfbe5cdbbb52429
issam-seghir <issamusma@hotmail.com>
2024-06-04 12:11:10 +0100

    setup clerk auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
@@ -15,8 +16,10 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
-			<body className={open_sans.className}>{children}</body>
-		</html>
+		<ClerkProvider>
+			<html lang="en">
+				<body className={open_sans.className}>{children}</body>
+			</html>
+		</ClerkProvider>
 	);
 }
---
+++ src/middleware.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+
+// protect all routes except the public one
+export default clerkMiddleware(
+	(auth, request) => {
+		if (!isPublicRoute(request)) {
+			auth().protect();
+		}
+	},
+	{ debug: process.env.NODE_ENV === "development" }
+);
+
+export const config = {
+	// The following matcher runs middleware on all routes
+	// except static assets.
+	matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
