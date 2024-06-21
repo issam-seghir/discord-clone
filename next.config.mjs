4b00f7fb88ab06dbc12a462fca763ff7f5777ba9
issam-seghir <issamusma@hotmail.com>
2024-06-21 11:18:51 +0100

    add image preview

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,11 @@ const nextConfig = {
 				hostname: "img.clerk.com",
 				port: "",
 			},
+			{
+				protocol: "https",
+				hostname: "utfs.io",
+				// pathname: `/a/${process.env.UPLOADTHING_APP_ID}/*`,
+			},
 		],
 	},
 };
--- src/components/modals/initial-model.tsx
+++ src/components/modals/initial-model.tsx
@@ -1,42 +1,42 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
 } from "@/components/ui/dialog";
-import {zodResolver} from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { UploadDropzone } from "@/lib/uploadthing";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Image from "next/image";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import { Form,FormControl,FormDescription ,FormField,FormItem,FormLabel,FormMessage} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { UploadButton, UploadDropzone } from "@/lib/uploadthing";
-
 
 export function InitialModel() {
-
 	const schema = z.object({
-		name: z.string().min(1,{message: "Server name is required"}),
-		imageUrl: z.string().min(1,{message: "Image URL is invalid"}),
+		name: z.string().min(1, { message: "Server name is required" }),
+		imageUrl: z.string().min(1, { message: "Image URL is invalid" }),
 	});
 	const form = useForm({
 		resolver: zodResolver(schema),
 		defaultValues: {
 			name: "",
 			imageUrl: "",
-
 		},
-
 	});
 
-	const {register, handleSubmit, formState} = form;
-	const isLoading =  formState.isSubmitting;
+	const { register, handleSubmit, formState, watch } = form;
+	const image = watch("imageUrl");
+	console.log(image);
 
-	const onSubmit = async (values : z.infer<typeof schema>) => {
+	const isLoading = formState.isSubmitting;
+
+	const onSubmit = async (values: z.infer<typeof schema>) => {
 		console.log(values);
 	};
 	return (
@@ -58,28 +58,40 @@ export function InitialModel() {
 									render={({ field }) => (
 										<FormItem>
 											<FormControl>
-												<UploadDropzone
-													className="mt-4  focus-visible:outline-zinc-700
+												{field?.value && field?.value?.split(".").pop() !== "pdf" ? (
+													<div className="relative h-20 w-20">
+														<Image
+															fill
+															objectFit="cover"
+															src={field.value}
+															className="rounded-full"
+															alt="Server Image"
+														/>
+													</div>
+												) : (
+													<UploadDropzone
+														className="mt-4  focus-visible:outline-zinc-700
 													focus-visible:outline-dashed
 													ut-button:bg-indigo-500 ut-button:text-white ut-button:hover:bg-indigo-500/90 ut-button:ut-readying:bg-indigo-500/90 ut-button:ut-uploading:bg-indigo-500/90 ut-button:after:bg-indigo-700
 													ut-label:text-zinc-700 ut-allowed-content:text-zinc-500
 													"
-													endpoint="serverImage"
-													onClientUploadComplete={(res) => {
-														field.onChange(res?.[0].url);
-														console.log("Files: ", res);
-														alert("Upload Completed");
-													}}
-													onUploadError={(error: Error) => {
-														console.log("UploadthingERROR\n", error.message);
+														endpoint="serverImage"
+														onClientUploadComplete={(res) => {
+															field.onChange(res?.[0].url);
+															console.log("Files: ", res);
+															alert("Upload Completed");
+														}}
+														onUploadError={(error: Error) => {
+															console.log("UploadthingERROR\n", error.message);
 
-														alert(`ERROR! ${error.message}`);
-													}}
-													onUploadBegin={(name) => {
-														// Do something once upload begins
-														console.log("Uploading: ", name);
-													}}
-												/>
+															alert(`ERROR! ${error.message}`);
+														}}
+														onUploadBegin={(name) => {
+															// Do something once upload begins
+															console.log("Uploading: ", name);
+														}}
+													/>
+												)}
 											</FormControl>
 											<FormMessage />
 										</FormItem>
