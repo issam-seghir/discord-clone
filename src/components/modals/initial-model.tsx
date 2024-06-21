97648253c257dd7a17554bd13075d65e042af29d
issam-seghir <issamusma@hotmail.com>
2024-06-21 11:32:20 +0100

    add remove photo button + style

--- src/components/modals/initial-model.tsx
+++ src/components/modals/initial-model.tsx
@@ -67,6 +67,25 @@ export function InitialModel() {
 															className="rounded-full"
 															alt="Server Image"
 														/>
+														<Button
+															onClick={() => field.onChange("")}
+															className="w-7 h-7  p-[.35rem] absolute bg-rose-500 hover:bg-rose-800 text-white top-0 right-0 rounded-full"
+														>
+															<svg
+																xmlns="http://www.w3.org/2000/svg"
+																className="h-6 w-6 "
+																fill="none"
+																viewBox="0 0 24 24"
+																stroke="currentColor"
+															>
+																<path
+																	strokeLinecap="round"
+																	strokeLinejoin="round"
+																	strokeWidth={2}
+																	d="M6 18L18 6M6 6l12 12"
+																/>
+															</svg>
+														</Button>
 													</div>
 												) : (
 													<UploadDropzone
