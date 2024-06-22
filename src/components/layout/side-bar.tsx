91f2d6c0161d6d6f0adacd622ea5af93dea91af7
issam-seghir <issamusma@hotmail.com>
2024-06-22 18:46:01 +0100

    add mode toggler and user button to the sidebar

--- src/components/layout/side-bar.tsx
+++ src/components/layout/side-bar.tsx
@@ -6,6 +6,8 @@ import { SideBarActions } from "@/components/layout/side-bar-actions";
 import { Separator } from "@/components/ui/separator";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { SideBarItem } from "@/components/layout/side-bar-item";
+import { ModeToggle } from "@/components/mode-toggler";
+import { UserButton } from "@clerk/nextjs";
 
 
 export  async function SideBar() {
@@ -19,7 +21,7 @@ const servers = await getProfileServers(profile.id);
 	return (
 		<div className="flex flex-col space-y-4 items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
 			<SideBarActions />
-			<Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto"/>
+			<Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
 			<ScrollArea className="flex-1 w-full">
 				{servers?.map((server) => (
 					<div key={server.id} className="mb-4">
@@ -27,7 +29,17 @@ const servers = await getProfileServers(profile.id);
 					</div>
 				))}
 			</ScrollArea>
-
+			<div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+				<ModeToggle />
+				<UserButton
+					afterSignOutUrl="/"
+					appearance={{
+						elements: {
+							avatarBox: "h-[48px] w-[48px]",
+						},
+					}}
+				/>
+			</div>
 		</div>
 	);
 }
