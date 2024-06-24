f3b8f5de3827d9d62e38a1a9256dcd80c19c8588
issam-seghir <issamusma@hotmail.com>
2024-06-24 18:23:41 +0100

    fix sidebar light mode

--- src/components/layout/side-bar.tsx
+++ src/components/layout/side-bar.tsx
@@ -18,7 +18,7 @@ export  async function SideBar() {
 	const servers = await getAllServers(profile.id);
 
 	return (
-		<div className="flex flex-col space-y-4 items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
+		<div className="flex flex-col space-y-4 items-center h-full text-primary w-full bg-[#E3E5E8] dark:bg-[#1E1F22] py-3">
 			<SideBarActions />
 			<Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
 			<ScrollArea className="flex-1 w-full">
