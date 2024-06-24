94616790f5bab3b6b8c6e03d0cb5b869270a053b
issam-seghir <issamusma@hotmail.com>
2024-06-24 19:36:38 +0100

    redirect to chat when clicked in server member

--- src/components/server-member.tsx
+++ src/components/server-member.tsx
@@ -23,9 +23,12 @@ export  function ServerMember({ member, server }: ServerMemberProps) {
     const router = useRouter()
 
     const icon = roleIconMap[member.role];
+	const onClick = () => {
+		router.push(`servers/${params?.serverId}/conversations/${member.id}`);
+	};
 	return (
 		<button
-			onClick={() => {}}
+			onClick={onClick}
 			className={cn(
 				"group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
 				params?.memberId === member.id && "bg-zinc-700/10 dark:bg-zinc-700"
@@ -35,7 +38,7 @@ export  function ServerMember({ member, server }: ServerMemberProps) {
 			<p
 				className={cn(
 					"font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition",
-					params?.channelId === member.id && "text-primary dark:text-zinc-200 dark:group-hover:text-white"
+					params?.memberId === member.id && "text-primary dark:text-zinc-200 dark:group-hover:text-white"
 				)}
 			>
 				{member.profile?.name}
