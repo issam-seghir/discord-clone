54ff619e32b5f4262477b71d72ee41c8a784d222
issam-seghir <issamusma@hotmail.com>
2024-06-25 12:18:19 +0100

    add pdf upload logic

--- src/components/modals/message-file-modal.tsx
+++ src/components/modals/message-file-modal.tsx
@@ -9,26 +9,26 @@ import {
 	DialogHeader,
 	DialogTitle,
 } from "@/components/ui/dialog";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
 import { UploadDropzone } from "@/lib/uploadthing";
 import { zodResolver } from "@hookform/resolvers/zod";
 import axios from "axios";
 import Image from "next/image";
+import qs from "query-string";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import qs from "query-string";
 
-import { useRouter } from "next/navigation";
 import { useStore } from "@/store/store";
+import { FileIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export function MessageFileModal() {
 	const router = useRouter();
-    const type = useStore.use.type();
+	const type = useStore.use.type();
 	const isOpen = useStore.use.isOpen();
 	const onClose = useStore.use.onClose();
 	const data = useStore.use.data();
-    const isModalOpen = isOpen && type === "messageFile";
+	const isModalOpen = isOpen && type === "messageFile";
 
 	const schema = z.object({
 		fileUrl: z.string().min(1, { message: "Attachment is invalid" }),
@@ -47,10 +47,10 @@ export function MessageFileModal() {
 	const onSubmit = async (values: z.infer<typeof schema>) => {
 		try {
 			const url = qs.stringifyUrl({
-				url: data?.apiUrl || '',
+				url: data?.apiUrl || "",
 				query: data?.query,
 			});
-			await axios.post(url, {...values ,content: values.fileUrl});
+			await axios.post(url, { ...values, content: values.fileUrl });
 			router.refresh();
 			form.reset();
 			handleClose();
@@ -58,10 +58,10 @@ export function MessageFileModal() {
 			console.log(error);
 		}
 	};
-    const handleClose = () => {
-        form.reset();
-        onClose();
-    }
+	const handleClose = () => {
+		form.reset();
+		onClose();
+	};
 	return (
 		<Dialog open={isModalOpen} onOpenChange={handleClose}>
 			<DialogContent className="bg-white text-black p-0 overflow-hidden">
@@ -111,29 +111,48 @@ export function MessageFileModal() {
 															</svg>
 														</Button>
 													</div>
-												) : (
+												) : !field?.value ? (
 													<UploadDropzone
-														className="mt-4  focus-visible:outline-zinc-700
-													focus-visible:outline-dashed
-													ut-button:bg-indigo-500 ut-button:text-white ut-button:hover:bg-indigo-500/90 ut-button:ut-readying:bg-indigo-500/90 ut-button:ut-uploading:bg-indigo-500/90 ut-button:after:bg-indigo-700
-													ut-label:text-zinc-700 ut-allowed-content:text-zinc-500
-													"
+														className="mt-4 focus-visible:outline-zinc-700 focus-visible:outline-dashed ut-button:bg-indigo-500 ut-button:text-white ut-button:hover:bg-indigo-500/90 ut-button:ut-readying:bg-indigo-500/90 ut-button:ut-uploading:bg-indigo-500/90 ut-button:after:bg-indigo-700 ut-label:text-zinc-700 ut-allowed-content:text-zinc-500"
 														endpoint="messageFile"
 														onClientUploadComplete={(res) => {
 															field.onChange(res?.[0].url);
 															console.log("Files: ", res);
 															alert("Upload Completed");
 														}}
-														onUploadError={(error: Error) => {
-															console.log("UploadthingERROR\n", error.message);
-
-															alert(`ERROR! ${error.message}`);
-														}}
-														onUploadBegin={(name) => {
-															// Do something once upload begins
-															console.log("Uploading: ", name);
-														}}
 													/>
+												) : (
+													<div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+														<FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
+														<a
+															href={field?.value}
+															target="_blank"
+															rel="noopener noreferrer"
+															className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
+														>
+															{field?.value}
+														</a>
+														<Button
+															onClick={() => field.onChange("")}
+															type="button"
+															className="w-7 h-7  p-[.35rem] absolute bg-rose-500 hover:bg-rose-800 text-white -top-2 -right-2 rounded-full shadow-sm"
+														>
+															<svg
+																xmlns="http://www.w3.org/2000/svg"
+																className="h-6 w-6 "
+																fill="none"
+																viewBox="0 0 24 24"
+																stroke="currentColor"
+															>
+																<path
+																	strokeLinecap="round"
+																	strokeLinejoin="round"
+																	strokeWidth={2}
+																	d="M6 18L18 6M6 6l12 12"
+																/>
+															</svg>
+														</Button>
+													</div>
 												)}
 											</FormControl>
 											<FormMessage />
@@ -143,7 +162,7 @@ export function MessageFileModal() {
 							</div>
 						</div>
 						<DialogFooter className="bg-gray-100  px-6 py-4">
-							<Button  type="submit" variant="primary" disabled={isLoading} className="w-full sm:w-20">
+							<Button type="submit" variant="primary" disabled={isLoading} className="w-full sm:w-20">
 								Send
 							</Button>
 						</DialogFooter>
