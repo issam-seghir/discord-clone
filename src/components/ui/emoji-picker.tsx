f486a9d5feb97fb6700621056f4888e4d4d09666
issam-seghir <issamusma@hotmail.com>
2024-06-25 14:26:59 +0100

    dynamic  emojipicker theme

--- src/components/ui/emoji-picker.tsx
+++ src/components/ui/emoji-picker.tsx
@@ -3,18 +3,20 @@ import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover
 import data from "@emoji-mart/data";
 import Picker from "@emoji-mart/react";
 import { Smile } from "lucide-react";
+import { useTheme } from "next-themes";
 
 interface EmojiPickerProps {
 	onChange: (value: string) => void;
 }
 
 export function EmojiPicker({ onChange }: EmojiPickerProps) {
+    const {resolvedTheme} = useTheme();
 	return (
 		<Popover>
 			<PopoverTrigger>
 				<Smile
 					className="text-zinc-500 dark:text-zinc-400 hover:text-zinc-600 dark:hover:text-zinc-300 transition"
-					size={16}
+					size={20}
 				/>
 			</PopoverTrigger>
 			<PopoverContent
@@ -22,7 +24,7 @@ export function EmojiPicker({ onChange }: EmojiPickerProps) {
 				sideOffset={40}
 				className="bg-transparent border-none shadow-none drop-shadow-none mb-16"
 			>
-				<Picker data={data} onEmojiSelect={(emoji: any) => onChange(emoji.native)} />
+				<Picker theme={resolvedTheme} data={data} onEmojiSelect={(emoji: any) => onChange(emoji.native)} />
 			</PopoverContent>
 		</Popover>
 	);
